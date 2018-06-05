f95116a01969985200d118a4db700c939d10ce80
tudi <superdi_cdi@163.com>
2018-06-05 19:30:36 +0800

    修复小bug

--- ZhiHu/main.py
+++ ZhiHu/main.py
@@ -3,5 +3,5 @@ from scrapy import cmdline
 __author__ = "TuDi"
 __date__ = "2018/3/22 下午10:31"
 
-cmdline.execute("scrapy crawl zhihu".split())
+cmdline.execute("scrapy crawl zhihu_login".split())
 
